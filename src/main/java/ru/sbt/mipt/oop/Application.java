759354f2ae7ce66f396d79ee36d09ab4ca6d10e4
Vitaly Krasnov <krasnov.vv@phystech.edu>
2020-03-25 21:37:08 +0300

    add 4 hometask

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -1,20 +1,18 @@
 package ru.sbt.mipt.oop;
 
 import java.util.Arrays;
+import com.coolcompany.smarthome.events.SensorEventsManager;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 
 public class Application {
 
     public static void main(String... args) {
-        SmartHome smartHome = SmartHomeReader.readSmartHome();
 
-        AlarmProcessEventDecorator alarmProcessEventDecorator =
-                new AlarmProcessEventDecorator(Arrays.asList(new DoorEventProcessor(),
-                        new LightEventProcessor(), new HallDoorEvenProcessor(), new AlarmEventProcessor()));
 
-        SmartHomeHandler homeHandler = new SmartHomeHandler(Arrays.asList(alarmProcessEventDecorator),
-                new EventCreatorImpl());
-
-        homeHandler.handle(smartHome);
+        AbstractApplicationContext context = new AnnotationConfigApplicationContext(SmartHomeConfiguration.class);
+        SensorEventsManager sensorEventsManager = context.getBean(SensorEventsManager.class);
+        sensorEventsManager.start();
     }
 
 }
